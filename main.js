b4727d06b951615e0d66ae2e0ad57d9bfb3d42fc
zzeongho <junghoo022@gmail.com>
2023-03-27 16:26:23 +0900

    monday

--- main.js
+++ main.js
@@ -49,10 +49,10 @@ function onClickAdd() {
 }
 
 function onClickRegist() {
-  const quotesMsg = document.querySelector(".quotes.Msg");
-  const newQuotes = document.querySelector(".newQuotesInput");
-
+  const quotesMsg = document.querySelector(".quotesMsg");
+  const newQuotes = document.querySelector(".newQuotes");
   const newQuotesInput = document.querySelector(".newQuotesInput");
+
   if (!newQuotesInput.value) {
     return;
   }
@@ -61,8 +61,74 @@ function onClickRegist() {
 
   let quotesArray = JSON.parse(savedQuotes);
   quotesArray.push(newQuotesInput.value);
+
   localStorage.setItem(QUOTES, JSON.stringify(quotesArray));
 
-  quotesMsg.innerHTML = `<span>${newQuotesInput.value}<span>`;
+  quotesMsg.innerHTML = `<span style="color:red;">${newQuotesInput.value}</span>`;
   newQuotes.style.display = "none";
+  newQuotesInput.value = "";
+}
+
+// const a = {
+//   question: "질문입니다.",
+// };
+
+// const b = {
+//   question: question,
+// };
+
+// const c = {
+//   question,
+// };
+
+let isLoading = false;
+
+async function onClickSearch() {
+  const searchInput = document.querySelector(".searchInput");
+  const searchResult = document.querySelector(".searchResult");
+
+  if (!searchInput.value) return;
+  if (isLoading) return;
+
+  //여기까지옴 == isloading이 false라는것
+
+  isLoading = true;
+  const question = searchInput.value;
+
+  searchInput.value = "검색 중 입니다... 잠시만 기다려주세요.";
+
+  // 프론트엔드에서 백엔드
+  const response = await axios.post(
+    "https://holy-fire-2749.fly.dev/chat",
+    {
+      question,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer BLOCKCHAINSCHOOL3",
+      },
+    }
+  );
+
+  if (response.status === 200) {
+    searchResult.style.display = "inline";
+    searchResult.innerText = response.data.choices[0].message.content;
+  }
+
+  searchInput.value = "";
+  isLoading = false;
+}
+
+function onClickToggle(value) {
+  const nft = document.querySelector(".nft");
+  const nftView = document.querySelector(".nftView");
+
+  if (value) {
+    nft.style.display = "inline-block";
+    nftView.style.display = "none";
+  } else {
+    nft.style.display = "none";
+    nftView.style.display = "inline-block";
+  }
 }
